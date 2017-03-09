2752944e88b210f285785d2a1102934641213574
Keane Patterson <keane.patterson@gmail.com>
2017-03-08 19:56:41 -0600

    Recolored obsidian and molten armor to their correct color.

--- src/main/java/com/kp/kpsgearmod/item/ItemMoltenChestplate.java
+++ src/main/java/com/kp/kpsgearmod/item/ItemMoltenChestplate.java
@@ -2,9 +2,14 @@ package com.kp.kpsgearmod.item;
 
 import com.kp.kpsgearmod.KPsGearMod;
 import com.kp.kpsgearmod.Reference;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
 /**
  * Created by keane on 3/8/2017.
@@ -19,5 +24,11 @@ public class ItemMoltenChestplate extends ItemArmor
         setRegistryName(Reference.KPsGearModItems.MOLTENCHESTPLATE.getRegistryName());
         setCreativeTab(KPsGearMod.tabKPsGearMod);
         setMaxStackSize(1);
+    }
+
+    @Override
+    public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
+    {
+        player.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 100, 2));
     }
 }
